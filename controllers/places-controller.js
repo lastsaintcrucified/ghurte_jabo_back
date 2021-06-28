9ccbfb1d76f24d9bdbb9ef7ba75155b2f89b287c
lastsaintcrucified <lastsaintcrucified@gmail.com>
2021-06-28 11:05:30 +0600

    backend complete

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -85,7 +85,7 @@ const createPlace = async (req, res, next) => {
   });
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new httpError("Creating place failed,please try again", 500);
     return next(error);
